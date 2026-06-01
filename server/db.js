cc994f6c166319c211d0e5a8b6dcb8599b8d54cf
indracits <indrajithij802@gmail.com>
2026-06-01 19:58:50 +0530

    db changes

--- server/db.js
+++ server/db.js
@@ -3,12 +3,19 @@ const crypto = require('crypto');
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log('DB Config:', {
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD ? '***' : undefined,
+  DB_NAME: process.env.DB_NAME,
+  DB_PORT: process.env.DB_PORT,
+});
 const {
-  DB_HOST = '127.0.0.1',
-  DB_USER = 'root',
-  DB_PASSWORD = '',
-  DB_NAME = 'cicd_db',
-  DB_PORT = 3306,
+  DB_HOST ,
+  DB_USER ,
+  DB_PASSWORD, 
+  DB_NAME ,
+  DB_PORT 
 } = process.env;
 
 function hashPassword(password) {
