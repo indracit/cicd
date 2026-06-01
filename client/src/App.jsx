c59c44452e042f72e31c761bac3f47f836c6b682
indracits <indrajithij802@gmail.com>
2026-06-01 19:43:54 +0530

    api url changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
     setLoading(true)
     try {
-      const response = await fetch('/auth', {
+      const response = await fetch('/api/auth', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
